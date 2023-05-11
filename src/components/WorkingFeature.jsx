6b0ada223c77166ac308ef2e3a28d619b540ee8b
Kinfe123 <kinfishtech@gmail.com>
2023-05-11 13:07:38 +0300

    Typo Again :)

--- src/components/WorkingFeature.jsx
+++ src/components/WorkingFeature.jsx
@@ -27,7 +27,7 @@ const WorkingFeature = () => (
   <div className="features section__padding" id="features">
     <div className="features-heading">
       <h1 className="gradient__text">
-        The Future is Now and You Just Need to Realize It. Step Into Future
+        The Future is Now and You Just Need to Realize It. Step Into The Future
         Today. & Make it Happen.
       </h1>
       <p>Request Early Access to Get Started</p>
